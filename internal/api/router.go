3e0d36d635dab95c650c846dc4e8d9d77e43185f
Ali-Nur <alinursagynbaev31@gmail.com>
2025-12-12 22:58:56 +0600

    fixed CORS and caseCamel in model

--- internal/api/router.go
+++ internal/api/router.go
@@ -8,11 +8,20 @@ import (
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
 	"gorm.io/gorm"
 )
 
 func NewRouter(db *gorm.DB) *chi.Mux {
 	r := chi.NewRouter()
+	r.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: true,
+		MaxAge:           300,
+	}))
 
 	r.Use(middleware.RequestID)
 	r.Use(middleware.RealIP)
--- internal/model/experiment.go
+++ internal/model/experiment.go
@@ -5,10 +5,10 @@ import "time"
 type Experiment struct {
 	ID               uint      `gorm:"primaryKey" json:"id"`
 	Title            string    `gorm:"uniqueIndex; size:255; not null" json:"title"`
-	ShortDescription string    `gorm:"size:255; not null" json:"short_description"`
-	FullContent      string    `gorm:"not null" json:"full_content"`
-	ImageUrl         *string   `json:"image_url"`
-	VideoUrl         *string   `json:"video_url"`
+	ShortDescription string    `gorm:"size:255; not null" json:"shortDescription"`
+	FullContent      string    `gorm:"not null" json:"fullContent"`
+	ImageUrl         *string   `json:"imageUrl"`
+	VideoUrl         *string   `json:"videoUrl"`
 	Category         string    `gorm:"size:255; not null" json:"category"`
-	CreatedAt        time.Time `json:"created_at"`
+	CreatedAt        time.Time `json:"createdAt"`
 }
